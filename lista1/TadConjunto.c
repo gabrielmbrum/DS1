b672a41662b234a2e5a7c3babe6717717b64cb60
peter parker <gabrielbrum2805@gmail.com>
2024-04-05 16:21:20 -0300

    organizing the code

--- lista1/TadConjunto.c
+++ lista1/TadConjunto.c
@@ -3,8 +3,10 @@
 #include "TadConjunto.h"
 
 int main () {
+    // fields
     int *arr1, *arr2, *res, inp;
 
+    // initializing and implementing arr1 and arr2
     printf("Digite o tamanho do array 1: ");
     scanf(" %d", &inp);
     arr1 = malloc(sizeof(int) * inp);
@@ -23,35 +25,31 @@ int main () {
         scanf(" %d", &arr2[i]);
     }
 
-
     res = Uniao(arr1, arr2);
-
-    printf("tam res: %d\n", (int)(sizeof(res)/sizeof(int)));
     
     printf("\nPrintando resultado!\n");
-    for (int i = 0; i <= (sizeof(arr1) + sizeof(arr2))/sizeof(int); i++) {
+
+    for (int i = 0; res[i]; i++) {
         printf("res[%d]: %d\n", i, res[i]);
     }
 }
 
 int* Uniao (int *a, int *b) {
-    int *arr, sizeA = sizeof(a)/sizeof(int), sizeB = sizeof(b)/sizeof(int);
-    int len = sizeA + sizeB + 1, i;
+    // fields
+    int *arr, sizeA, sizeB, len, i;
 
-    
+    // initializing vars
+    sizeA = sizeof(a)/sizeof(int);
+    sizeB = sizeof(b)/sizeof(int);
+    len = sizeA + sizeB + 1;
     arr = malloc(sizeof(int) * len);
 
-    for (i = 0; i <= len; i++) {
-        printf("arr[%d]: %d\n", i, arr[i]);
-    }
-    printf("len: %d\n", len);
-    printf("arr: %d\n", (int)(sizeof(arr)/sizeof(int)));
-    for (i = 0; i <= sizeA; i++) {
+    //implementing returning array with a and b elements
+    for (i = 0; i <= sizeA; i++) 
         arr[i] = a[i];
-    }
 
-    for (i = 0; i <= sizeB; i++) {
+    for (i = 0; i <= sizeB; i++) 
         arr[sizeA+i] = b[i];
-    }
+
     return arr;
 }
