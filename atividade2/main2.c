81a5492fab20ac78c729eaf12b3c7611fdbb5bf2
peter parker <gabrielbrum2805@gmail.com>
2024-04-19 20:35:56 -0300

    fixing stupid stuff that i made

---
+++ atividade2/main2.c
@@ -0,0 +1,17 @@
+#include "AVL.h"
+
+int main () {
+    no *raiz = cria_no(5), *seis = cria_no(6), *sete = cria_no(7),*tres = cria_no(3), *quatro = cria_no(4);
+
+    raiz = insere(raiz, sete);
+    raiz = insere(raiz, quatro);
+    raiz = insere(raiz, seis);
+    raiz = insere(raiz, tres);
+    printf("\nimprimindo árvore:\n");
+    preOrd(raiz);
+
+    raiz = remover(raiz, 3);
+
+    printf("\nimprimindo árvore pós remoção:\n");
+    preOrd(raiz);
+}
